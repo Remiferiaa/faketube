086088ddae0e96a95958c67ec72e207137e65ad5
Remiferiaa <slong819@hotmail.com>
2022-06-08 22:41:38 +0800

    Split format between view and sub numbers

--- src/Utils/numFormat.ts
+++ src/Utils/numFormat.ts
@@ -1,8 +1,15 @@
-const nForm = (x: number) => {
+const subForm = (x: number) => {
     if (1e9 < x) {return +(x/1e9).toFixed(2) + 'B'}
     if (1e9 > x && x > 1e6 ) {return +(x/1e6).toFixed(2) + 'M'}
-    if (1e6 > x && x > 1e3 ) {return +(x/1e6).toFixed(2) + 'K'}
+    if (1e6 > x && x > 1e3 ) {return +(x/1e3).toFixed(2) + 'K'}
     if (1e3 > x) {return x}
 }
 
-export default nForm
+const viewForm = (x: number) => {
+    if (1e9 < x) {return +Math.floor(x/1e9) + 'B'}
+    if (1e9 > x && x > 1e6 ) {return +Math.floor(x/1e6) + 'M'}
+    if (1e6 > x && x > 1e3 ) {return +Math.floor(x/1e3) + 'K'}
+    if (1e3 > x) {return x}
+}
+
+export {subForm, viewForm}
