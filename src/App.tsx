1709c68e12c3e0849b713905fe52f678297b4f99
Remiferiaa <slong819@hotmail.com>
2022-06-13 23:37:38 +0800

    added state change for sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import {useState} from 'react'
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Popular from "./Components/Watch/Popular";
 import Header from "./Components/Header/Head";
 import Result from "./Components/Search/Results";
 import Vid from "./Components/Watch/Video";
+import SideBar from "./Components/Sidebar/Side";
 
 function App() {
+  const [sideState, setSide] = useState<string>('Active')
   return (
     <Router basename="/">
-      <Header />
+      <Header sideState={sideState} setSideState={setSide}/>
+      <SideBar/>
       <Routes>
         <Route path="/" element={<Popular />}></Route>
         <Route path="/results" element={<Result />}></Route>
