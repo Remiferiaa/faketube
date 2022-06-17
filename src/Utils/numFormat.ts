97efab56388c7a2957b8599809bc9308314a97d8
Remiferiaa <slong819@hotmail.com>
2022-06-18 01:34:57 +0800

    fix error for numbers that is divisible by 10

--- src/Utils/numFormat.ts
+++ src/Utils/numFormat.ts
@@ -1,8 +1,12 @@
 const subForm = (x: number) => {
     let pattern = /(\d{1,3}).(\d{1,3})/
     if (1e9 < x) {
-        let y = String(x / 1e9).match(pattern)!
-        if (y[1].length < 3) {
+        let unitValue = x / 1e9
+        let y = String(unitValue).match(pattern)!
+        if (unitValue % 1 === 0) {
+            return `${unitValue}B`
+        }
+        else if (y[1].length < 3 && Number(y[2]) !== 0) {
             let endPoint = 3 - y[1].length
             return `${y[1]}.${y[2].slice(0, endPoint)}B`
         } else {
@@ -10,8 +14,12 @@ const subForm = (x: number) => {
         }
     }
     if (1e9 > x && x > 1e6) {
-        let y = String(x / 1e6).match(pattern)!
-        if (y[1].length < 3) {
+        let unitValue = x / 1e6
+        let y = String(unitValue).match(pattern)!
+        if (unitValue % 1 === 0) {
+            return `${unitValue}M`
+        }
+        else if (y[1].length < 3 && Number(y[2]) !== 0) {
             let endPoint = 3 - y[1].length
             return `${y[1]}.${y[2].slice(0, endPoint)}M`
         } else {
@@ -19,8 +27,12 @@ const subForm = (x: number) => {
         }
     }
     if (1e6 > x && x > 1e3) {
-        let y = String(x / 1e3).match(pattern)!
-        if (y[1].length < 3) {
+        let unitValue = x / 1e3
+        let y = String(unitValue).match(pattern)!
+        if (unitValue % 1 === 0) {
+            return `${unitValue}K`
+        }
+        else if (y[1].length < 3 && Number(y[2]) !== 0) {
             let endPoint = 3 - y[1].length
             return `${y[1]}.${y[2].slice(0, endPoint)}K`
         } else {
@@ -31,25 +43,25 @@ const subForm = (x: number) => {
 }
 
 const viewForm = (x: number) => {
-    if (1e9 < x) { 
-        let results = x/1e9
-        if(results > 10) {
+    if (1e9 < x) {
+        let results = x / 1e9
+        if (results > 10) {
             return +Math.floor(results) + 'B'
         } else {
             return results.toFixed(1) + 'B'
         }
     }
-    if (1e9 > x && x > 1e6)  { 
-        let results = x/1e6
-        if(results > 10) {
+    if (1e9 > x && x > 1e6) {
+        let results = x / 1e6
+        if (results > 10) {
             return +Math.floor(results) + 'M'
         } else {
             return results.toFixed(1) + 'M'
         }
     }
-    if (1e6 > x && x > 1e3)  { 
-        let results = x/1e3
-        if(results > 10) {
+    if (1e6 > x && x > 1e3) {
+        let results = x / 1e3
+        if (results > 10) {
             return +Math.floor(results) + 'K'
         } else {
             return results.toFixed(1) + 'K'
